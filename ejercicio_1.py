721efd858031cecdd4811f4b71cfd4db74e4cde4
MarcoBaltazar <156382236+MarcoBaltazar@users.noreply.github.com>
2024-04-07 18:54:48 -0500

    Add files via upload

---
+++ ejercicio_1.py
@@ -0,0 +1,19 @@
+#duracion de cursos para aprender loq ue hemos aprendido hasta ahora con dalto
+min=2.5
+max=7.0
+pro=4.0
+este=1.5
+
+#sacamos la diferencia de porcentajes con otros cursos
+
+dif_min=100-este/min*100
+
+print(f"El curso de dalto es {dif_min}% mas rapido que el mas rapido")
+
+dif_pro=100-este/pro*100
+
+print(f"El curso de dalto es {dif_pro}% mas rapido que el promedio")
+
+dif_max=100-este/max*100
+
+print(f"El curso de dalto es {round(dif_max,3)}% mas rapido que el mas lento")
