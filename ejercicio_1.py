c428492997f095370e0c893de0eae6bdfb996861
MarcoBaltazar <156382236+MarcoBaltazar@users.noreply.github.com>
2024-04-07 22:07:45 -0500

    Update ejercicio_1.py

--- ejercicio_1.py
+++ ejercicio_1.py
@@ -1,8 +1,8 @@
 #duracion de cursos para aprender loq ue hemos aprendido hasta ahora con dalto
-min=3.5
+min=2.5
 max=7.0
 pro=4.0
-este=2.5
+este=1.5
 
 #sacamos la diferencia de porcentajes con otros cursos
 
